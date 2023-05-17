881202cbab1a9dc03b2adc97df74fcf1a35f8c01
Michał Plebański <michalplebanski@gmail.com>
2023-05-17 19:01:29 +0200

    add and display shop list

---
+++ main.py
@@ -0,0 +1,7 @@
+shop_list = {
+    "warzywaniak": ["buraki", "seler", "papryka"],
+    "spoywczy": ["masło", "mleko"]
+}
+
+for shop, product in shop_list.items():
+    print(f"Idę do {shop} kupić {product}")
