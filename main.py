77161dcbf802c046e99e782edf420eaf54b28469
Michał Plebański <michalplebanski@gmail.com>
2023-05-21 09:59:02 +0200

    add comment

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ shop_list = {
     "spoywczy": ["masło", "mleko"]
 }
 
+print("Lista zakupów:\n")
+
 for shop, product in shop_list.items():
     shopUpper = shop.capitalize()
     productUpper = [item.capitalize() for item in product]
     print(f"Idę do {shopUpper} kupić {productUpper}")
+
